70d84688c85e9d36772aa85e7eb9857c275966f0
Florence Delannoy <florence.delannoy@outlook.com>
2017-08-10 09:37:16 +0200

    exo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,13 +6,21 @@ class ReviewsController < ApplicationController
   end
 
   def create
-  	@review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+  	@restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
+private
+
 	def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category: 	  "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category: 	  "belgian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category: 	  "italian"
+  },
+   {
+    name:         "Mamma",
+    address:      "rue d'enghien 75010 Paris",
+    category:     "italian"
+  },
+  {
+    name:         "Shanghai",
+    address:      "villa gaudelt 75011 Paris",
+    category: 	  "chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
